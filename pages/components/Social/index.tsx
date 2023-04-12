81d406ff05d61d3926ecf701f8dad5edd0043a10
Nguyễn Văn Đức <ducnd49@vtp-ducnv49.local>
2023-04-12 09:37:20 +0700

    update

--- pages/components/Social/index.tsx
+++ pages/components/Social/index.tsx
@@ -26,7 +26,7 @@ const Social = () => {
     <Popover
       className="social-popover"
       content={content}
-      placement="left"
+      placement="topLeft"
       trigger="click"
     >
       <Button type="primary">
