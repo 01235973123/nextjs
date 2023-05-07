50e0037c0947a571c5b16c2228e14a9d9a2fe52e
Duc Nguyen Van <duc.nguyen3@amela.vn>
2023-05-07 23:07:27 +0700

    update

--- pages/ho-tro/index.tsx
+++ pages/ho-tro/index.tsx
@@ -30,7 +30,7 @@ const SupportVtp = () => {
     // const [menuActive, setMenuActive] = useState(0);
     const [loading, setLoading] = useState(false);
     const [errorField, setErrorField] = useState(null);
-    const errorFieldRef = useRef<any>(null);
+    const errorFieldRef = useRef<any>('name');
 
     useEffect(() => {
         if (errorFieldRef.current) {
